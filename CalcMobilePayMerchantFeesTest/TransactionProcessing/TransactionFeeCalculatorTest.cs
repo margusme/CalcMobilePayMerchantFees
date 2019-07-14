b3a3fc8fcb78b7e1c32cf848916f5619fd64873b
Margus Metskivi <margus.metskivi@gmail.com>
2019-07-15 02:48:22 +0300

    MOBILEPAY-5. Extended unit tests for new methods

--- CalcMobilePayMerchantFeesTest/TransactionProcessing/TransactionFeeCalculatorTest.cs
+++ CalcMobilePayMerchantFeesTest/TransactionProcessing/TransactionFeeCalculatorTest.cs
@@ -50,5 +50,30 @@ namespace CalcMobilePayMerchantFeesTest.TransactionProcessing
             result = calculator.CalculateAdditionalFirstDayFee(transactionObject);
             result.ShouldBe(0.00m);
         }
+
+        [TestMethod]
+        public void TestMerchantHasTransactionsForMonthShouldReturnCorrectValue()
+        {
+            var calculator = new TransactionFeeCalculator();
+
+            var transactionObject = new TransactionObject() { TransactionDate = new DateTime(2018, 10, 1), MerchantName = "SEVEN", TransactionAmount = 0 };
+
+            var result = TransactionFeeCalculatorChild.MerchantHasTransactionsForMonth(null);
+            result.ShouldBe(true);
+
+            result = TransactionFeeCalculatorChild.MerchantHasTransactionsForMonth(transactionObject);
+            result.ShouldBe(false);
+
+            result = TransactionFeeCalculatorChild.MerchantHasTransactionsForMonth(transactionObject);
+            result.ShouldBe(true);
+        }
+    }
+
+    public class TransactionFeeCalculatorChild : TransactionFeeCalculator
+    {
+        public new static bool MerchantHasTransactionsForMonth(TransactionObject transactionObject)
+        {
+            return TransactionFeeCalculator.MerchantHasTransactionsForMonth(transactionObject);
+        }
     }
 }
