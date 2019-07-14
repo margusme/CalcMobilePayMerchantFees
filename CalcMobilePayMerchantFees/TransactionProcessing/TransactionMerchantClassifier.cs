ec3afcade76b61c15ca8451b1b7b3ec7f2e3ee3d
Margus Metskivi <margus.metskivi@gmail.com>
2019-07-14 23:28:09 +0300

    MOBILEPAY-3. Telia merchant will get Transaction Fee Percentage Discount 10% per transaction. Added unit tests for covering new situation and new transaction fee calculator for Telia

--- CalcMobilePayMerchantFees/TransactionProcessing/TransactionMerchantClassifier.cs
+++ CalcMobilePayMerchantFees/TransactionProcessing/TransactionMerchantClassifier.cs
@@ -14,6 +14,16 @@ namespace CalcMobilePayMerchantFees.TransactionProcessing
         /// <returns>Payment transaction fee for given merchant. At the moment returns the result for all payment transactions the same way</returns>
         public static decimal CalculateTransactionFeeByMerchant(TransactionObject transactionObject)
         {
+            if (transactionObject == null)
+            {
+                return 0;
+            }
+
+            if (transactionObject.MerchantName.ToUpper().Equals(TransactionFeeCalculatorTelia.MerchantName))
+            {
+                return TransactionFeeCalculatorTelia.CalculateTransactionFee(transactionObject);
+            }
+
             return TransactionFeeCalculator.CalculateTransactionFee(transactionObject);
         }
     }
